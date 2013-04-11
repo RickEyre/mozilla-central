416fdfacd6a429f9fc4f415737b706a43494525c
JSilver99 <jasilver1@learn.senecac.on.ca>
2013-04-11 01:57:23 -0400

    Moved adding the caption overlay child div to TextTrackCue.

--- content/media/TextTrackCue.h
+++ content/media/TextTrackCue.h
@@ -239,6 +239,7 @@ public:
 
 private:
   void CueChanged();
+  void CreateCueOverlay();
 
   nsCOMPtr<nsISupports> mGlobal;
   nsString mText;
@@ -256,6 +257,9 @@ private:
   nsString mVertical;
   double mLine;
   TextTrackCueAlign mAlign;
+
+  // Anonymous child which is appended to VideoFrame's caption display div.
+  nsCOMPtr<nsIContent> mCueDiv;
 };
 
 } // namespace dom
