7f286c3861c722dd787e3312f262091e6a5852f4
Rick Eyre <rick.eyre@hotmail.com>
2013-08-08 14:29:55 -0400

    Bug 895091 - Add WebVTT parser and wrapper IDls r=rillian, ted, gwagner, khuey
    - Added vtt.js, a JS WebVTT parser, to Gecko.
    - Added a wrapper to interface between it and Gecko.
    - Updated TextTrackCue and WebVTTListener to work with it.

--- content/media/TextTrackCue.h
+++ content/media/TextTrackCue.h
@@ -11,7 +11,8 @@
 #include "mozilla/dom/VTTCueBinding.h"
 #include "nsCycleCollectionParticipant.h"
 #include "nsDOMEventTargetHelper.h"
-#include "nsIDocument.h"
+#include "nsIWebVTTParserWrapper.h"
+#include "mozilla/StaticPtr.h"
 
 namespace mozilla {
 namespace dom {
@@ -288,6 +289,8 @@ public:
    */
   already_AddRefed<DocumentFragment> GetCueAsHTML();
 
+  void SetTrackElement(HTMLTrackElement* aTrackElement);
+
 private:
   void CueChanged();
   void SetDefaultCueSettings();
@@ -317,6 +320,8 @@ private:
   // anytime a property that relates to the display of the TextTrackCue is
   // changed.
   bool mReset;
+
+  static StaticRefPtr<nsIWebVTTParserWrapper> sParserWrapper;
 };
 
 } // namespace dom
