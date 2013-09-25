b099b0f9af3587b808615f45a37b8651354a771f
Rick Eyre <rick.eyre@hotmail.com>
2013-09-25 10:22:50 -0400

    Bug 865407 - Part 6: Move update display of cues code to TextTrackManager

--- content/html/content/src/TextTrackManager.cpp
+++ content/html/content/src/TextTrackManager.cpp
@@ -7,6 +7,12 @@
 
 #include "mozilla/dom/TextTrackManager.h"
 #include "mozilla/dom/HTMLMediaElement.h"
+#include "mozilla/dom/TextTrackRegion.h"
+#include "mozilla/ClearOnShutdown.h"
+#include "nsComponentManagerUtils.h"
+#include "nsVideoFrame.h"
+#include "nsIFrame.h"
+#include "nsTArrayHelpers.h"
 
 namespace mozilla {
 namespace dom {
@@ -15,11 +21,19 @@ NS_IMPL_CYCLE_COLLECTION_1(TextTrackManager, mTextTracks)
 NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(TextTrackManager, AddRef)
 NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(TextTrackManager, Release)
 
+StaticRefPtr<nsIWebVTTParserWrapper> TextTrackManager::sParserWrapper;
+
 TextTrackManager::TextTrackManager(HTMLMediaElement *aMediaElement)
   : mMediaElement(aMediaElement)
 {
   MOZ_COUNT_CTOR(TextTrackManager);
   mTextTracks = new TextTrackList(mMediaElement->OwnerDoc()->GetParentObject());
+
+ nsresult rv;
+  nsCOMPtr<nsIWebVTTParserWrapper> parserWrapper =
+    do_CreateInstance(NS_WEBVTTPARSERWRAPPER_CONTRACTID, &rv);
+  sParserWrapper = parserWrapper;
+  ClearOnShutdown(&sParserWrapper);
 }
 
 TextTrackManager::~TextTrackManager()
@@ -58,10 +72,71 @@ TextTrackManager::DidSeek()
   mTextTracks->DidSeek();
 }
 
-void
-TextTrackManager::Update(double aTime)
+bool
+TextTrackManager::GetDirtyCues(nsTArray<nsRefPtr<TextTrackCue> >& aCues)
 {
-  mTextTracks->Update(aTime);
+  mTextTracks->GetAllActiveCues(aCues);
+  for (uint32_t i = 0; i < aCues.Length(); i++) {
+    if (aCues[i]->Reset()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+template<class T> JS::Value
+TextTrackManager::ArrayToJSArray(const nsTArray<T>& aArray)
+{
+  AutoPushJSContext cx(
+    nsContentUtils::GetContextFromDocument(mMediaElement->OwnerDoc()));
+
+  JSObject* jsArray;
+  nsTArrayToJSArray(cx, aArray, &jsArray);
+
+  return JS::ObjectValue(*jsArray);
+}
+
+void
+TextTrackManager::UpdateCueDisplay()
+{
+  nsIFrame* frame = mMediaElement->GetPrimaryFrame();
+  if (!frame) {
+    return;
+  }
+
+  nsVideoFrame* videoFrame = do_QueryFrame(frame);
+  if (!videoFrame) {
+    return;
+  }
+
+  nsIContent* overlay =  videoFrame->GetCaptionOverlay();
+  if (!overlay) {
+    return;
+  }
+
+  nsTArray<nsRefPtr<TextTrackCue> > activeCues;
+  if (GetDirtyCues(activeCues)) {
+
+    nsTArray<nsRefPtr<TextTrackRegion> > regions;
+    mTextTracks->GetAllRegions(regions);
+
+    JS::Value jsCues = ArrayToJSArray<nsRefPtr<TextTrackCue> >(activeCues);
+    JS::Value jsRegions = ArrayToJSArray<nsRefPtr<TextTrackRegion> >(regions);
+
+    nsPIDOMWindow* window = mMediaElement->OwnerDoc()->GetWindow();
+    if (window) {
+      sParserWrapper->ProcessCues(window, jsCues, jsRegions, overlay);
+    }
+  }
+
+  nsContentUtils::SetNodeTextContent(overlay, EmptyString(), true);
+
+  nsGenericHTMLElement* element = static_cast<nsGenericHTMLElement*>(overlay);
+  nsCOMPtr<nsIDOMNode> throwAway;
+  for (uint32_t i = 0; i < activeCues.Length(); i++) {
+    element->AppendChild(activeCues[i]->DisplayState(),
+                         getter_AddRefs(throwAway));
+  }
 }
 
 } // namespace dom
